132b6f76df7f8d0de3af01aaa9eacc86da356010
Siddharth Singh <siddharth.s1312@gmail.com>
2018-07-14 07:50:11 +0530

    introducing props to call the weatherApi

--- src/App.js
+++ src/App.js
@@ -4,12 +4,30 @@ import Titles from "./components/Titles";
 import Form from "./components/Form";
 import Weather from "./components/Weather";
 
+const API_KEY = "uSeyOurOwnapIkey";
+const BASE_URL = "http://api.openweathermap.org/data/2.5/weather";
+
 class App extends React.Component{
+
+	getWeatherFromOWM = async (e) => {
+		e.preventDefault();
+
+		var city =  e.target.elements.city.value;
+		var country = e.target.elements.country.value;
+
+		var extendedUrl = `?q=${city},${country}&appid=${API_KEY}&units=metric`;
+
+		const response = await fetch(BASE_URL+extendedUrl);
+		const data = await response.json();
+
+		console.log(data);
+	}
+
 	render(){
 		return(
 			<div>
 				<Titles />
-				<Form />
+				<Form getWeatherDetails={this.getWeatherFromOWM}/>
 				<Weather />
 			</div>
 		);
--- src/components/Form.js
+++ src/components/Form.js
@@ -3,7 +3,7 @@ import React from "react";
 class Form extends React.Component{
 	render(){
 		return(
-			<form>
+			<form onSubmit={this.props.getWeatherDetails}>
 				<input type="text" name="city" placeholder="City" />
 				<input type="text" name="country" placeholder="Country" />
 				<button>Get Weather Details</button>
