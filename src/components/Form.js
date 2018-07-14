38a869818439d1c396a70dcb5f3dd513d859b9b0
Siddharth Singh <siddharth.s1312@gmail.com>
2018-07-14 07:13:50 +0530

    Adding a form

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,7 +3,11 @@ import React from "react";
 class Form extends React.Component{
 	render(){
 		return(
-			<div>Need implementation</div>
+			<form>
+				<input type="text" name="city" placeholder="City" />
+				<input type="text" name="country" placeholder="Country" />
+				<button>Get Weather Details</button>
+			</form>
 		);
 	}
 }
