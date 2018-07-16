adbc00e5bfc21bbf70c981f0a6893409bf4b5ccb
Siddharth Singh <siddharth.s1312@gmail.com>
2018-07-16 11:20:42 +0530

    Updating readme

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const Form = props => (
 			<form className="login" onSubmit={props.getWeatherDetails}>
-				<input type="text" name="city" placeholder="City" />
-				<input type="text" name="country" placeholder="Country" />
+				<input type="text" name="city" placeholder="City" autocomplete="off" />
+				<input type="text" name="country" placeholder="Country" autocomplete="off" />
 				<input type="submit" value="Get Weather Details" className="btn btn-success btn-sm" />
 			</form>	
 	);
